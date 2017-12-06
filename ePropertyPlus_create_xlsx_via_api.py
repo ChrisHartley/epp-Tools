ea2c90f047241951af7a854128e5b0429fc6ffcd
Chris Hartley <chris.hartley@anymouse.org>
2017-12-06 11:40:59 -0500

    switch to Requests, put API key in header, refactor for code-reuse

--- ePropertyPlus_create_xlsx_via_api.py
+++ ePropertyPlus_create_xlsx_via_api.py
@@ -3,9 +3,7 @@
 ### inventory spreadsheet available for users to download.
 ### GPL v3 license, by Chris Hartley, Renew Indianapolis.
 ### chris.hartley@renewindianapolis.org
-from urllib2 import urlopen
-from urllib import quote
-import json
+from requests import Request, Session
 import xlsxwriter
 from os.path import getmtime
 import datetime
@@ -18,12 +16,22 @@ try:
 except ImportError:
     API_KEY = ''
 
-API_ENDPOINT = 'https://indysbx.epropertyplus.com/landmgmt/api/property/summary/'
+API_ENDPOINT = 'https://indysbx.epropertyplus.com/landmgmt/api/'
 
 # Edit the query as necessary
+api_resource = 'property/summary'
+
 json_query = '{"criterias":[{"name":"active","value":"Yes","operator":"EQUALS"},{"name":"published","value":"Yes","operator":"EQUALS"}]}'
+
+parameters = {
+    'page': 1,
+    'limit': 50000,
+    'json': json_query,
+    }
+
+
 FILENAME = '/tmp/Inventory-Export.xlsx'
-REFRESH_SECONDS = 300
+REFRESH_SECONDS = 3
 # Define the fields to write to the spreadsheet.
 # Column name, field name from response json.
 fields = (
@@ -44,18 +52,31 @@ if tdelta.total_seconds() > REFRESH_SECONDS: # 5 minutes
     workbook = xlsxwriter.Workbook(FILENAME)
     worksheet = workbook.add_worksheet('Available Inventory')
 
-    url = '{0}?x-strllc-authkey={1}&json={2}&page=1&limit=50000'.format(API_ENDPOINT,API_KEY,quote(json_query))
-    response = urlopen(url)
-    json_obj = json.load(response)
+    url = '{0}{1}'.format(API_ENDPOINT, api_resource,)
+    s = Session()
+    headers = {
+        'Content-Type': 'application/json',
+        'x-strllc-authkey':  API_KEY,
+        'Accept': 'application/json',
+    }
+    s.headers.update(headers)
+    r = s.get(url, params=parameters)
+    json_obj = r.json()
+    if json_obj['success'] == True:
 
-    # Write column names across the first row.
-    for indx,field in enumerate(fields):
-        worksheet.write(0, indx, field[0])
-
-    # For each record returned, write the fields we care about to the spreadsheet.
-    for row,record in enumerate(json_obj['rows'], start=1):
+        # Write column names across the first row.
         for indx,field in enumerate(fields):
-            worksheet.write(row, indx, record[field[1]])
+            worksheet.write(0, indx, field[0])
+
+        # For each record returned, write the fields we care about to the spreadsheet.
+        for row,record in enumerate(json_obj['rows'], start=1):
+            for indx,field in enumerate(fields):
+                worksheet.write(row, indx, record[field[1]])
+    else:
+        print "Endpoint returned success = false"
+        print r.url
+        print r.headers
+        print r.text
     workbook.close()
 else:
     print 'File cached, not re-fetching.'
